818667c15327eda658101c0ad946f46e7220b145
drhead <a@a.a>
2023-06-27 19:49:38 +0000

    Lowered repeat_interleave

--- segment_anything/modeling/mask_decoder_hq.py
+++ segment_anything/modeling/mask_decoder_hq.py
@@ -169,10 +169,15 @@ class MaskDecoderHQ(nn.Module):
         output_tokens = output_tokens.unsqueeze(0).expand(sparse_prompt_embeddings.size(0), -1, -1)
         tokens = torch.cat((output_tokens, sparse_prompt_embeddings), dim=1)
 
+        def xla_repeat_interleave(input, repeats, dim):
+            shape = list(input.shape)
+            shape.insert(dim, repeats)
+            return input.unsqueeze(dim + 1).expand(*shape).reshape(-1, *shape[2:])
+
         # Expand per-image data in batch direction to be per-mask
-        src = torch.repeat_interleave(image_embeddings, tokens.shape[0], dim=0)
+        src = xla_repeat_interleave(image_embeddings, tokens.shape[0], dim=0)
         src = src + dense_prompt_embeddings
-        pos_src = torch.repeat_interleave(image_pe, tokens.shape[0], dim=0)
+        pos_src = xla_repeat_interleave(image_pe, tokens.shape[0], dim=0)
         b, c, h, w = src.shape
 
         # Run the transformer
