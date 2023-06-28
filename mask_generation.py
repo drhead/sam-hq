ddf492f64794967bfdfaee51d4d23e4a75875d98
drhead <a@a.a>
2023-06-28 09:22:32 +0000

    works single threaded

--- mask_generation.py
+++ mask_generation.py
@@ -53,7 +53,7 @@ class CustomImageDataset(Dataset):
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.file_list[idx])
         image = cv2.imread(img_path)
-        print(img_path)
+        # print(img_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         if self.transform:
             image = self.transform(image)
@@ -62,6 +62,7 @@ class CustomImageDataset(Dataset):
 data_directory = "./data/zd_testimgs/"
 dataset = CustomImageDataset(data_directory)
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=0)
+pl_loader = pl.ParallelLoader(dataloader,xm.get_xla_supported_devices("TPU",8))
 
 sam_checkpoint = "pretrained_checkpoint/sam_hq_vit_l.pth"
 model_type = "vit_l"
@@ -81,15 +82,16 @@ if trace:
     server = xp.start_server(6009)
     time.sleep(5)
 
-SERIAL_EXEC = xmp.MpSerialExecutor()
-def load_dataset():
-    return dataloader
+# SERIAL_EXEC = xmp.MpSerialExecutor()
+# def load_dataset():
+#    return dataloader
     # dataset = CustomImageDataset(path)
     # return DataLoader(dataset, batch_size=32, shuffle=True, num_workers=0)
 
 def _mp_fn(index):
     print(f"Spawned process for index {index}")
-    dist.init_process_group('xla', init_method='pjrt://')
+    if __name__ != '__main__':
+        dist.init_process_group('xla', init_method='pjrt://')
     model = WRAPPED_MODEL.to(xm.xla_device())
 
     print(f"Model moved to device on {index}")
@@ -104,14 +106,14 @@ def _mp_fn(index):
         points_per_batch=16
     )
     print(f"Model loaded on {index}")
-    train_loader = SERIAL_EXEC.run(lambda: load_dataset())
+    train_loader = pl_loader.per_device_loader(xm.xla_device()) # SERIAL_EXEC.run(lambda: load_dataset())
     print(f"Dataset loaded on {index}")
     for batch_idx, (input) in enumerate(train_loader):
         print(f"On device {index}, starting batch {batch_idx}")
-
+        input=input.cpu().numpy()
         for i in range(input.shape[0]):
-            masks = mask_generator.generate(input[i, ...].numpy(), multimask_output=False)
+            masks = mask_generator.generate(input[i], multimask_output=False)
             print(f"On device {index}, batch {batch_idx}, got {masks.shape[0]} masks")
 
 if __name__ == '__main__':
-    xmp.spawn(_mp_fn, args=(), start_method='fork')
+    xmp.spawn(_mp_fn, args=(), nprocs=1, start_method='fork')
